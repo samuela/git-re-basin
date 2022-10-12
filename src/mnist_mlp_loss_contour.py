3eaf29c23c1f61cb393ce1c91d3904005fe2e3ed
Samuel Ainsworth <skainsworth@gmail.com>
2022-10-12 04:07:34 +0000

    paper version of mnist_mlp_loss_contour.py

--- src/mnist_mlp_loss_contour.py
+++ src/mnist_mlp_loss_contour.py
@@ -1,8 +1,8 @@
-import urllib
 from pathlib import Path
 
 import jax.numpy as jnp
 import matplotlib
+import matplotlib.colors as colors
 import matplotlib.pyplot as plt
 import numpy as np
 import wandb
@@ -16,26 +16,28 @@ from tqdm import tqdm
 
 import matplotlib_style as _
 from mnist_mlp_train import MLPModel, load_datasets, make_stuff
-from utils import (ec2_get_instance_type, flatten_params, lerp, timeblock, unflatten_params)
-from weight_matching import (apply_permutation, mlp_permutation_spec, weight_matching)
+from utils import (ec2_get_instance_type, flatten_params, lerp, timeblock,
+                   unflatten_params)
+from weight_matching import (apply_permutation, mlp_permutation_spec,
+                             weight_matching)
 
 matplotlib.rcParams["text.usetex"] = True
 
 with wandb.init(
     project="git-re-basin",
     entity="skainswo",
-    tags=["mnist", "mlp", "weight-matching", "barrier-vs-epoch"],
+    tags=["mnist", "mlp", "weight-matching", "loss-contour"],
     job_type="analysis",
 ) as wandb_run:
   # api = wandb.Api()
-  # seed0_run = api.run("skainswo/playing-the-lottery/1b1gztfx")
-  # seed1_run = api.run("skainswo/playing-the-lottery/1hrmw7wr")
+  # seed0_run = api.run("skainswo/git-re-basin/1b1gztfx")
+  # seed1_run = api.run("skainswo/git-re-basin/1hrmw7wr")
 
   config = wandb.config
   config.ec2_instance_type = ec2_get_instance_type()
   config.epoch = 99
   config.seed = 123
-  config.num_eval_points = 1024
+  config.num_eval_points = 2048
 
   model = MLPModel()
   stuff = make_stuff(model)
@@ -110,31 +112,65 @@ with wandb.init(
   # Linearly interpolate the data (x, y) on a grid defined by (xi, yi).
   triang = tri.Triangulation(eval_points[:, 0], eval_points[:, 1])
   # We need to cap the maximum loss value so that the contouring is not completely saturated by wildly large losses
-  interpolator = tri.LinearTriInterpolator(triang, jnp.minimum(4, eval_results[:, 0]))
+  interpolator = tri.LinearTriInterpolator(triang, jnp.minimum(0.55, eval_results[:, 0]))
+  # interpolator = tri.LinearTriInterpolator(triang, jnp.log(jnp.minimum(1.5, eval_results[:, 0])))
   zi = interpolator(*np.meshgrid(xi, yi))
 
   plt.figure()
-  plt.contour(xi, yi, zi, levels=20, linewidths=0.25, colors="k")
-  # cmap = "RdGy"
-  cmap = "RdYlBu_r"
-  # cmap ="Spectral_r"
-  # cmap = "coolwarm"
-  # cmap = "RdBu_r"
-  plt.contourf(xi, yi, zi, levels=20, cmap=cmap, extend="both")
+  num_levels = 13
+  plt.contour(xi, yi, zi, levels=num_levels, linewidths=0.25, colors="grey", alpha=0.5)
+  # cmap_name = "RdGy"
+  # cmap_name = "RdYlBu"
+  # cmap_name = "Spectral"
+  cmap_name = "coolwarm_r"
+
+  # cmap_name = "YlOrBr_r"
+  # cmap_name = "RdBu"
+
+  # See https://stackoverflow.com/a/18926541/3880977
+  def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+    return colors.LinearSegmentedColormap.from_list(
+        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
+        cmap(np.linspace(minval, maxval, n)))
+
+  cmap = truncate_colormap(plt.get_cmap(cmap_name), 0.0, 0.9)
+  plt.contourf(xi, yi, zi, levels=num_levels, cmap=cmap, extend="both")
 
   x, y = project2d(model_a_flat)
-  plt.scatter([x], [y], marker="x", color="lightgrey", label="model_a")
+  plt.scatter([x], [y], marker="x", color="white", zorder=10)
 
   x, y = project2d(model_b_flat)
-  plt.scatter([x], [y], marker="x", color="lightgrey", label="model_b")
+  plt.scatter([x], [y], marker="x", color="white", zorder=10)
 
   x, y = project2d(model_b_rebasin_flat)
-  plt.scatter([x], [y], marker="x", color="lightgrey", label="model_b_rebasin")
+  plt.scatter([x], [y], marker="x", color="white", zorder=10)
 
-  plt.text(-0.15, -0.15, r"${\bf \Theta_A}$", color="white", fontsize=24)
-  plt.text(1.055, -0.15, r"${\bf \Theta_B}$", color="white", fontsize=24)
+  label_bboxes = dict(facecolor="tab:grey", boxstyle="round", edgecolor="none", alpha=0.5)
+  plt.text(-0.075,
+           -0.1,
+           r"${\bf \Theta_A}$",
+           color="white",
+           fontsize=24,
+           bbox=label_bboxes,
+           horizontalalignment="right",
+           verticalalignment="top")
+  plt.text(1.075,
+           -0.1,
+           r"${\bf \Theta_B}$",
+           color="white",
+           fontsize=24,
+           bbox=label_bboxes,
+           horizontalalignment="left",
+           verticalalignment="top")
   x, y = project2d(model_b_rebasin_flat)
-  plt.text(x - 0.325, y + 0.075, r"${\bf \pi(\Theta_B)}$", color="white", fontsize=24)
+  plt.text(x - 0.075,
+           y + 0.1,
+           r"${\bf \pi(\Theta_B)}$",
+           color="white",
+           fontsize=24,
+           bbox=label_bboxes,
+           horizontalalignment="right",
+           verticalalignment="bottom")
 
   # https://github.com/matplotlib/matplotlib/issues/17284#issuecomment-772820638
   # Draw line only
@@ -232,6 +268,8 @@ with wandb.init(
   # plt.colorbar()
   plt.xlim(-0.4, 1.4)
   plt.ylim(-0.45, 1.3)
+  #   plt.xlim(-0.9, 1.9)
+  #   plt.ylim(-0.9, 1.9)
   plt.xticks([])
   plt.yticks([])
   plt.tight_layout()
